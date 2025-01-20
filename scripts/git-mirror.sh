b880ec0dc74aeeba5ebdb38a70fd48fe9445074d
Ondrej Kurák <ondrej@certora.com>
2025-01-20 13:52:20 +0100

    Update script
    41b594ddcf476dec3b17719baf7d18dcf5ac1d2a

--- scripts/git-mirror.sh
+++ scripts/git-mirror.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-set -x
+set -xe
 
 LAST_PUBLIC_HASH="$(git --git-dir="$1/.git" rev-list -1 HEAD)"
 LAST_PUBLIC_MESSAGE="$(git --git-dir="$1/.git" log --format=%B -n 1 "$LAST_PUBLIC_HASH")"
 PRIVATE_ORIGIN_HASH="$(echo "$LAST_PUBLIC_MESSAGE" | tail -1)"
+REVISIONS="$(git rev-list --reverse "${PRIVATE_ORIGIN_HASH}..HEAD")"
 
-for commit in $(git rev-list --reverse "${PRIVATE_ORIGIN_HASH}..HEAD"); do
-    git format-patch -k -1 --stdout "$commit" | git --git-dir="$1/.git" am --exclude tests_delete --exclude cloud-test-private -3 -k
-    OLD_MESSAGE="$(git --git-dir="$1/.git" log --format=%B -n 1 "$commit")"
-    git --git-dir="$1/.git" commit --amend -m "$OLD_MESSAGE" -m "$commit"
+OLD_DIR="$(pwd)"
+cd "$1" || exit 1
+
+for commit in $REVISIONS; do
+    git --git-dir="$OLD_DIR/.git" format-patch -k -1 --stdout "$commit" | git am --exclude tests_delete --exclude cloud-test-private -3 -k
+    OLD_MESSAGE="$(git log --format=%B -n 1 HEAD)"
+    # Add private hash to the commit message
+    git commit --amend -m "$OLD_MESSAGE" -m "$commit"
 done
