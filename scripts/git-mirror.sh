2f4abbbe483e02a6c07dabc409018fe0b5d248da
Ondrej Kurák <ondrej@certora.com>
2025-01-20 13:26:04 +0100

    Add mirror script
    21612e4fcf14c1cca7a6467ade7ec71706e3e202

---
+++ scripts/git-mirror.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x
+
+LAST_PUBLIC_HASH="$(git --git-dir="$1/.git" rev-list -1 HEAD)"
+LAST_PUBLIC_MESSAGE="$(git --git-dir="$1/.git" log --format=%B -n 1 "$LAST_PUBLIC_HASH")"
+PRIVATE_ORIGIN_HASH="$(echo "$LAST_PUBLIC_MESSAGE" | tail -1)"
+
+for commit in $(git rev-list --reverse "${PRIVATE_ORIGIN_HASH}..HEAD"); do
+    git format-patch -k -1 --stdout "$commit" | git --git-dir="$1/.git" am -3 -k
+    OLD_MESSAGE="$(git --git-dir="$1/.git" log --format=%B -n 1 "$commit")"
+    git --git-dir="$1/.git" commit --amend -m "$OLD_MESSAGE" -m "$commit"
+done
