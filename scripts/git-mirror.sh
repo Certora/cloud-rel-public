d563df521ca475086cbcbe734f3018bf3f6470d4
Ondrej Kurák <ondrej@certora.com>
2025-01-20 13:27:32 +0100

    Add changes
    d4a5b9d9c086a87b82f7d141f7dc708220c53b7b

--- scripts/git-mirror.sh
+++ scripts/git-mirror.sh
@@ -7,7 +7,7 @@ LAST_PUBLIC_MESSAGE="$(git --git-dir="$1/.git" log --format=%B -n 1 "$LAST_PUBLI
 PRIVATE_ORIGIN_HASH="$(echo "$LAST_PUBLIC_MESSAGE" | tail -1)"
 
 for commit in $(git rev-list --reverse "${PRIVATE_ORIGIN_HASH}..HEAD"); do
-    git format-patch -k -1 --stdout "$commit" | git --git-dir="$1/.git" am -3 -k
+    git format-patch -k -1 --stdout "$commit" | git --git-dir="$1/.git" am --exclude tests_delete --exclude cloud-test-private -3 -k
     OLD_MESSAGE="$(git --git-dir="$1/.git" log --format=%B -n 1 "$commit")"
     git --git-dir="$1/.git" commit --amend -m "$OLD_MESSAGE" -m "$commit"
 done
