f4d86ecf5cf3d78ed8b928760e7f2a58464b5084
Ondrej Kurák <ondrej@certora.com>
2025-01-21 12:42:20 +0000

    Test git diff
    fcf1d6713702ca6831a83cc515c24b7faab1fbd0

--- scripts/git-mirror.sh
+++ scripts/git-mirror.sh
@@ -9,20 +9,21 @@ REVISIONS="$(git rev-list --reverse "${PRIVATE_ORIGIN_HASH}..HEAD")"
 
 OLD_DIR="$(pwd)"
 cd "$1" || exit 1
+old_revision="$PRIVATE_ORIGIN_HASH"
 
 for commit in $REVISIONS; do
-    AUTHOR="$(git --git-dir="$OLD_DIR/.git" log --format='%an' -n 1 "$commit")"
-    EMAIL="$(git --git-dir="$OLD_DIR/.git" log --format='%ae' -n 1 "$commit")"
+    # AUTHOR="$(git --git-dir="$OLD_DIR/.git" log --format='%an' -n 1 "$commit")"
+    # EMAIL="$(git --git-dir="$OLD_DIR/.git" log --format='%ae' -n 1 "$commit")"
     MESSAGE="$(git --git-dir="$OLD_DIR/.git" log --format=%B -n 1 "$commit")"
-    git config --global user.email "$EMAIL"
-    git config --global user.name "$AUTHOR"
-    git --git-dir="$OLD_DIR/.git" format-patch \
-        -k -1 \
-        --stdout "$commit" \
+    # git config --global user.email "$EMAIL"
+    # git config --global user.name "$AUTHOR"
+    git --git-dir="$OLD_DIR/.git" diff "${old_revision}..${commit}" \
         -- . ':!tests_delete/' ':!**/*.solc' ':!**/*.wasm' |
-        git apply
-
+        git apply --allow-empty
+    old_revision="$commit"
     # Add private hash to the commit message
     rm -rf tests_delete cloud-test-private
-    git commit --amend -m "$MESSAGE" -m "$commit"
+    echo "$commit" >>.git-private-commit
+    git add -A .
+    git commit -m "$MESSAGE" -m "$commit"
 done
